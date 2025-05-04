45a24378334a468d6f461a5367318193d15be963
Cormac <cormac_gudge@hotmail.com>
2025-05-05 00:26:14 +0100

    Move code to tic_tac_toe

--- tic_tac_toe/src/main.rs
+++ tic_tac_toe/src/main.rs
@@ -1,3 +1,20 @@
-fn main() {
-    println!("Hello, world!");
+fn start_game() {
+    let board: [[i8; 3]; 3] = [[0, 0, 0]; 3];
+    print_board(board);
+}
+
+fn print_board(board: [[i8; 3]; 3]) {
+    println!("{} {} {}", format(board[0][0]), format(board[0][1]), format(board[0][2]));
+    println!("{} {} {}", format(board[1][0]), format(board[1][1]), format(board[1][2]));
+    println!("{} {} {}", format(board[2][0]), format(board[2][1]), format(board[2][2]));
+}
+
+fn format(value: i8) -> &'static str {
+    if value == 1 {
+        return "[X]";
+    } else if value == 2 {
+        return "[Y]";
+    } else {
+        return "[ ]";
+    }
 }
