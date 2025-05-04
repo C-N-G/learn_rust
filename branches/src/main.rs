857d7676674481d4d8de56e331e019763574330c
Cormac <cormac_gudge@hotmail.com>
2025-05-05 00:26:27 +0100

    Finish branches project

--- branches/src/main.rs
+++ branches/src/main.rs
@@ -1,30 +1,40 @@
+// fn main() {
+//     let number = 7;
+
+//     if number < 5 {
+//         println!("condition was true");
+//     } else {
+//         println!("condition was false");
+//     }
+// }
+
+// fn main() {
+//     let number = 6;
+
+//     if number % 4 == 0 {
+//         println!("number is divisible by 4");
+//     } else if number % 3 == 0 {
+//         println!("number is divisible by 3");
+//     } else if number % 2 == 0 {
+//         println!("number is divisible by 2");
+//     } else {
+//         println!("number is not divisible by 4, 3, or 2");
+//     }
+// }
+
 fn main() {
-    let number = 3;
-
-    if number < 5 {
-        println!("condition was true");
+    let condition = true;
+    let number = if condition { 5 } else { 6 };
+    let x;
+    if condition {
+        x = 1;
     } else {
-        println!("condition was false");
+        x = 2;
     }
-}
+    // Note that Rust does not require x to be initially 
+    // declared with let mut in the second snippet. This is 
+    // because Rust can determine that x is only ever assigned once, 
+    // since only one branch of the if-statement will ever execute.
 
-fn start_game() {
-    let board: [[i8; 3]; 3] = [[0, 0, 0]; 3];
-    print_board(board);
-}
-
-fn print_board(board: [[i8; 3]; 3]) {
-    println!("{} {} {}", format(board[0][0]), format(board[0][1]), format(board[0][2]));
-    println!("{} {} {}", format(board[1][0]), format(board[1][1]), format(board[1][2]));
-    println!("{} {} {}", format(board[2][0]), format(board[2][1]), format(board[2][2]));
-}
-
-fn format(value: i8) -> &'static str {
-    if value == 1 {
-        return "[X]";
-    } else if value == 2 {
-        return "[Y]";
-    } else {
-        return "[ ]";
-    }
+    println!("The value of number is: {number} {x}");
 }
