b351075c0c92a12ca9635bd8eba6bfbaaa6a8dee
Cormac <cormac_gudge@hotmail.com>
2025-05-04 22:47:15 +0100

    Add branches project

--- functions/src/main.rs
+++ functions/src/main.rs
@@ -1,3 +1,49 @@
+// fn main() {
+//     println!("Hello, world!");
+
+//     another_function();
+// }
+
+// fn another_function() {
+//     println!("Another function.");
+// }
+
+// fn main() {
+//     another_function(5);
+// }
+
+// fn another_function(x: i32) {
+//     println!("The value of x is: {x}");
+// }
+
+// fn main() {
+//     print_labeled_measurement(5, 'h');
+// }
+
+// fn print_labeled_measurement(value: i32, unit_label: char) {
+//     println!("The measurement is: {value}{unit_label}");
+// }
+
+/**
+ * Expressions do not include ending semicolons
+ */
+
+//  fn five() -> i32 {
+//     5
+// }
+
+// fn main() {
+//     let x = five();
+
+//     println!("The value of x is: {x}");
+// }
+
 fn main() {
-    println!("Hello, world!");
+    let x = plus_one(5);
+
+    println!("The value of x is: {x}");
+}
+
+fn plus_one(x: i32) -> i32 {
+    x + 1
 }
